464f34f33c8ce9304e101548d0f66bfb3dc648fb
natalieparent9 <78802736+natalieparent9@users.noreply.github.com>
2024-10-01 22:36:45 +0930

    Fix annotations in mr_horse Work on mvmr_horse file

--- R/mr_horse.R
+++ R/mr_horse.R
@@ -1,50 +1,52 @@
 library(R2jags)
 library(rstan)
 library(coda)
+library(parallelly)
+
 
 mr_horse_model_jags = function() {
-  for (i in 1:N){
-    by[i] ~ dnorm(mu[i], 1/(sy[i] * sy[i]))
-    mu[i] = theta * bx0[i] + alpha[i]
-    bx[i] ~ dnorm(bx0[i], 1 / (sx[i] * sx[i]))
+  for (i in 1:N){                                # Where N is the number of variants (rows)
+    by[i] ~ dnorm(mu[i], 1/(sy[i] * sy[i]))      # Likelihood for by
+    mu[i] = theta * bx0[i] + alpha[i]            # Mean for bx0 distribution
+    bx[i] ~ dnorm(bx0[i], 1 / (sx[i] * sx[i]))   # Likelihood for bx
 
-    bx0[i] ~ dnorm(mx0 + (sqrt(vx0)/(tau * phi[i])) * rho[i] * alpha[i], 1 / ((1 - rho[i]^2) * vx0))
-    r[i] ~ dbeta(10, 10);T(, 1)
-    rho[i] = 2*r[i] -1
+    bx0[i] ~ dnorm(mx0 + (sqrt(vx0)/(tau * phi[i])) * rho[i] * alpha[i], 1 / ((1 - rho[i]^2) * vx0)) # Effect of the variant on the exposure
+    r[i] ~ dbeta(10, 10);T(, 1)                  # Correlation between alpha and bx0
+    rho[i] = 2*r[i] -1                           # Converts the truncated beta distribution parameter r[i] to a correlation parameter rho[i].
 
-    alpha[i] ~ dnorm(0, 1 / (tau * tau * phi[i] * phi[i]))
-    phi[i] = a[i] / sqrt(b[i])
-    a[i] ~ dnorm(0, 1);T(0, )
-    b[i] ~ dgamma(0.5, 0.5)
+    alpha[i] ~ dnorm(0, 1 / (tau * tau * phi[i] * phi[i]))  # Pleiotropic effects
+    phi[i] = a[i] / sqrt(b[i])                   # Scaling factor for each variant based on parameters a and b
+    a[i] ~ dnorm(0, 1);T(0, )                    # Parameter for phi[i]
+    b[i] ~ dgamma(0.5, 0.5)                      # Parameter for phi[i]
   }
 
-  c ~ dnorm(0, 1);T(0, )
-  d ~ dgamma(0.5, 0.5)
-  tau = c / sqrt(d)
+  c ~ dnorm(0, 1);T(0, )       # Parameter for tau
+  d ~ dgamma(0.5, 0.5)         # Parameter for tau
+  tau = c / sqrt(d)            # Controls the global level of shrinkage for alphas
 
-  vx0 ~ dnorm(0, 1);T(0, )
-  mx0 ~ dnorm(0, 1)
+  vx0 ~ dnorm(0, 1);T(0, )     # Variance for bx0 distribution
+  mx0 ~ dnorm(0, 1)            # Mean for bx0 distribution
 
-  theta ~ dunif(-10, 10)
+  theta ~ dunif(-10, 10)       # Causal effect of X on Y
 }
 
 
 mr_horse_model_stan = "
 data {
-  int<lower=0> N;        // Number of observations (SNPs)
-  vector[N] by;          // Observed effect estimates on Y (beta_y)
-  vector[N] bx;          // Observed effect estimates on X (beta_x)
+  int<lower=0> N;        // Number of variants (rows)
+  vector[N] by;          // Association between the variant and exposure
+  vector[N] bx;          // Association between the exposure and outcome
   vector[N] sy;          // Standard errors for by
   vector[N] sx;          // Standard errors for bx
 }
 
 parameters {
-  real theta;            // Causal effect
-  vector[N] alpha;       // Pleiotropic effects
-  vector[N] bx0;         // Latent effect estimates on X
+  real theta;            // Causal effect of X on Y
+  vector[N] alpha;       // Pleiotropic effects on the outcome
+  vector[N] bx0;         // Effect of the variant on the exposure
   vector<lower=0>[N] a;  // Parameter for phi[i]
   vector<lower=0>[N] b;  // Parameter for phi[i]
-  vector<lower=0, upper=1>[N] r;  // Truncated beta distribution parameter for correlation
+  vector<lower=0, upper=1>[N] r;  // Correlation between alpha and bx0
   real<lower=0> c;       // Parameter for tau
   real<lower=0> d;       // Parameter for tau
   real<lower=0> vx0;     // Variance for bx0 distribution
@@ -52,46 +54,60 @@ parameters {
 }
 
 transformed parameters {
-  vector<lower=0>[N] phi = a ./ sqrt(b);        // Scaling factor for each SNP based on parameters a and b
+  vector<lower=0>[N] phi = a ./ sqrt(b);        // Scaling factor for each variant based on parameters a and b
   vector[N] rho = 2 * r - 1;           // Converts the truncated beta distribution parameter r[i] to a correlation parameter rho[i].
-  vector[N] mu = theta * bx0 + alpha;  // Computes the mean effect on Y for each SNP
-  real<lower=0> tau = c / sqrt(d);              // Precision parameter
+  vector[N] mu = theta * bx0 + alpha;  // Computes the mean effect on Y for each variant
+  real<lower=0> tau = c / sqrt(d);              // Controls the global level of shrinkage for alphas
 
 }
 
 model {
   // Priors
-  theta ~ uniform(-10, 10);                    // Uniform prior for causal effect theta
-  alpha ~ normal(0, tau * phi);                // Normal prior for alpha
-  bx0 ~ normal(mx0 + (sqrt(vx0) / (tau * phi)) .* rho .* alpha, sqrt((1 - rho .* rho) * vx0)); // Normal prior for bx0
-  a ~ normal(0, 1);                            // Normal prior for a, truncated to be positive
-  b ~ gamma(0.5, 0.5);                         // Gamma prior for b
-  r ~ beta(10, 10);                            // Beta prior for r, truncated to [0, 1] in params section
-  c ~ normal(0, 1);                            // Normal prior for c, truncated to be positive
-  d ~ gamma(0.5, 0.5);                         // Gamma prior for d
-  vx0 ~ normal(0, 1);                          // Normal prior for vx0, truncated to be positive
-  mx0 ~ normal(0, 1);                          // Normal prior for mx0
+  theta ~ uniform(-10, 10);
+  alpha ~ normal(0, tau * phi);
+  bx0 ~ normal(mx0 + (sqrt(vx0) / (tau * phi)) .* rho .* alpha, sqrt((1 - rho .* rho) * vx0));
+  a ~ normal(0, 1);
+  b ~ gamma(0.5, 0.5);
+  r ~ beta(10, 10);
+  c ~ normal(0, 1);
+  d ~ gamma(0.5, 0.5);
+  vx0 ~ normal(0, 1);
+  mx0 ~ normal(0, 1);
 
   // Likelihood
   by ~ normal(mu, sy);                         // Likelihood for by (observed beta_y)
   bx ~ normal(bx0, sx);                        // Likelihood for bx (observed beta_x)
 }
 "
+mr_horse_model_stan = rstan::stan_model(model_code = mr_horse_model_stan) # compiles stan model
 
-#' Title
+
+#' Univariable MR-Horse method
+#'
+#' Causal effect estimation is performed in a Bayesian framework using a horseshoe shrinkage prior to account for both correlated and uncorrelated pleiotropic effects
 #'
 #' @param D Data frame containing betaY, betaYse, betaX and betaXse, or MRInput object
-#' @param no_ini Number of chains
+#' @param n.chains Number of chains
 #' @param variable.names Parameters to save estimates for
 #' @param n.iter Number of iterations (not including warmup)
 #' @param n.burnin Number of warmup iterations
 #' @param stan Fit the model using stan, default is to use JAGS
+#' @param cores Number of cores to use in parallel if running multiple chains, defaults to parallelly::availableCores()
 #'
-#' @return
+#' @return List
 #' @export
 #'
 #' @examples
-mr_horse = function(D, no_ini = 3, variable.names = "theta", n.iter = 10000, n.burnin = 10000, stan = FALSE){
+#' # Load example data
+#' data(data_ex)
+#'
+#' # Fit model with JAGS
+#' MREx = mr_horse(data_ex)
+#'
+#' # Check estimates
+#' MREx$MR_Estimate
+mr_horse = function(D, n.chains = 3, variable.names = "theta", n.iter = 10000, n.burnin = 10000, stan = FALSE, n.cores = parallelly::availableCores()){
+
   # Validate input
   if (is.data.frame(D)) {  # accepts data.frame, tibble, data.table
   } else if (inherits(D, "MRInput")) {
@@ -111,41 +127,44 @@ mr_horse = function(D, no_ini = 3, variable.names = "theta", n.iter = 10000, n.b
     stop("Error: D must contain columns: betaX, betaY, betaXse, betaYse")
   }
 
-  # Ensure at least the results for theta are saved
+  # Ensure at least the results for theta parameter are saved
   if("theta" %in% variable.names){
     variable.names = variable.names
   } else{
     variable.names = c("theta", variable.names)
   }
 
-
-
-
-
-
-
+  # Fit model
   if (stan == TRUE) {
-    print('stan')
+    fit = rstan::sampling(mr_horse_model_stan,
+                   pars = variable.names,
+                   data = list("N"=nrow(D), by=D$betaY, bx = D$betaX, sy = D$betaYse, sx = D$betaXse),
+                   iter = n.iter + n.burnin,
+                   warmup = n.burnin,
+                   chains = n.chains,
+                   # control = list(adapt_delta = 0.9, max_treedepth = 12),
+                   cores = n.cores)
+    mr.coda = rstan::As.mcmc.list(fit)
+
   } else if (stan == FALSE) {
-    print('jags')
-      # jags_fit = R2jags::jags(data = list(by = D$betaY, bx = D$betaX, sy = D$betaYse, sx = D$betaXse, N = length(D$betaY)),
-      #             parameters.to.save = variable.names,
-      #             n.chains = no_ini,
-      #             n.iter = n.burnin + n.iter,
-      #             n.burnin = n.burnin,
-      #             model.file = mr_horse_model)
+      fit = R2jags::jags.parallel(data = list(by = D$betaY, bx = D$betaX, sy = D$betaYse, sx = D$betaXse, N = length(D$betaY)),
+                  parameters.to.save = variable.names,
+                  n.chains = n.chains,
+                  n.iter = n.burnin + n.iter,
+                  n.burnin = n.burnin,
+                  model.file = mr_horse_model_jags,
+                  n.cluster = n.cores)
+      mr.coda = coda::as.mcmc(fit)
   } else {
       stop("Error: invalid input for argument stan, must be TRUE or FALSE")
   }
 
-
-
-  # mr.coda = coda::as.mcmc(jags_fit)
-  # mr_estimate = data.frame("Estimate" = round(unname(summary(mr.coda[, "theta"])$statistics[1]), 3),
-  #                          "SD" = round(unname(summary(mr.coda[, "theta"])$statistics[2]), 3),
-  #                          "2.5% quantile" = round(unname(summary(mr.coda[, "theta"])$quantiles[1]), 3),
-  #                          "97.5% quantile" = round(unname(summary(mr.coda[, "theta"])$quantiles[5]), 3),
-  #                          "Rhat" = round(unname(gelman.diag(mr.coda)$psrf[1]), 3))
-  # names(mr_estimate) = c("Estimate", "SD", "2.5% quantile", "97.5% quantile", "Rhat")
-  # return(list("MR_Estimate" = mr_estimate, "MR_Coda" = mr.coda))
+  mr_estimate = data.frame("Estimate" = summary(mr.coda[, "theta"])$statistics[[1]],
+                           "SD" = summary(mr.coda[, "theta"])$statistics[[2]],
+                           "2.5% quantile"  = summary(mr.coda[, "theta"])$quantiles[[1]],
+                           "97.5% quantile" = summary(mr.coda[, "theta"])$quantiles[[5]],
+                           "Rhat" = gelman.diag(mr.coda)$psrf[[1]])
+  mr_estimate = round(mr_estimate, 3)
+  names(mr_estimate) = c("Estimate", "SD", "2.5% quantile", "97.5% quantile", "Rhat")
+  return(list("MR_Estimate" = mr_estimate, "MR_Coda" = mr.coda))
 }
--- R/mvmr_horse.R
+++ R/mvmr_horse.R
@@ -1,3 +1,7 @@
+library(R2jags)
+library(rstan)
+library(coda)
+library(parallelly)
 
 
 mvmr_horse_model_jags = function() {
@@ -33,22 +37,89 @@ mvmr_horse_model_jags = function() {
   }
 }
 
+mvmr_horse_model_stan = "
+data {
+    int<lower=1> N;                    // Number of observations
+    int<lower=1> K;                    // Number of variants
+    matrix[N, K] bx;                   // Matrix of observed effect estimates on x
+    vector[N] by;                      // Vector of observed effect estimates on y
+    vector[N] sy;                      // Standard errors for by
+    matrix[K, K*N] Tx;                 // Covariance matrix for bx
+    matrix[K, K] R;                    // Prior precision matrix for mx, diagonal of 1s
+}
 
+parameters {
+    matrix[N, K] bx0;                  // Latent matrix of effect estimates on x
+    vector[K] theta;                   // Effect estimates on y
+    vector[N] alpha;                   // Random intercepts
+    vector<lower=0, upper=1>[N] r;     // Beta distribution parameter for rho
+    vector<lower=0>[N] a;              // Parameter for phi
+    vector<lower=0>[N] b;              // Parameter for phi
+    vector[K] mx;                      // Prior mean for bx0
+    vector<lower=0>[K] vx0;            // Variances for bx0
+    real<lower=0> c;                   // Parameter for tau
+    real<lower=0> d;                   // Parameter for tau
+}
+
+transformed parameters {
+    real tau = c / sqrt(d);                  // Tau is defined as c divided by sqrt(d)
+    vector[N] phi = a ./ sqrt(b);            // Scaling factor for each SNP based on parameters a and b, ./ is element wise division
+    vector[N] rho = 2 * r - 1;               // Converts the truncated beta distribution parameter r[i] to a correlation parameter rho[i]
+    vector[N] kappa = (rho^2 ./ (1 + K*rho^2));   // Used to adjust bx0
+    matrix[K, K] A = diag_matrix(1.0 ./ vx0);  // Diagonal precision matrix for bx0, diagonal elements are 1/vx0, off-diagonal are 0
+    matrix[K, K] B = (1.0 ./ sqrt(vx0)) * (1.0 ./ sqrt(vx0))'; // Matrix B for covariance
+    matrix[K, K] covariance_matrix[N];
+    // Convert precision matrix to covariance
+    for (i in 1:N) {
+      matrix[K, K] precision_matrix = A - kappa[i] * B; // covariance matrix for bx0
+      covariance_matrix[i] = inverse(precision_matrix);
+    }
+}
+
+model {
+    for (i in 1:N) {
+        // Likelihood
+        by[i] ~ normal(dot_product(bx0[i], theta) + alpha[i], sy[i]);
+        bx[i] ~ multi_normal(bx0[i], Tx[, (i-1)*K + 1:i*K]);
+
+        // Priors
+        alpha[i] ~ normal(0, tau * phi[i]);
+        bx0[i] ~ multi_normal(mx + sqrt(vx0) .* (rho[i] * alpha[i] / (phi[i] * tau)), covariance_matrix[i]);
+        r[i] ~ beta(10, 10);
+        a[i] ~ normal(0, 1);
+        b[i] ~ gamma(0.5, 0.5);
+    }
+
+    // Priors
+    c ~ normal(0, 1);
+    d ~ gamma(0.5, 0.5);
+
+    mx ~ multi_normal(rep_vector(0, K), R);
+
+    for (k in 1:K) {
+        vx0[k] ~ normal(0, 1);
+        theta[k] ~ uniform(-10, 10);
+    }
+}
+"
 
 
 #' Title
 #'
 #' @param D Dataset containing betaY, betaYse and at least betaX1, betaX1se, betaX2 and betaX2se
-#' @param no_ini Number of chains
+#' @param n.chains Number of chains
 #' @param variable.names Parameters to save estimates for
 #' @param n.iter Number of iterations (not including warmup)
 #' @param n.burnin Number of warmup iterations
+#' @param n.chains Number of cores to use in parallel if running multiple chains, defaults to parallelly::availableCores()
 #'
 #' @return
 #' @export
 #'
 #' @examples
-mvmr_horse = function(D, no_ini = 3, variable.names = "theta", n.iter = 10000, n.burnin = 10000){
+mvmr_horse = function(D, n.chains = 3, variable.names = "theta", n.iter = 10000, n.burnin = 10000, n.cores = parallelly::availableCores()){
+
+
   if("theta" %in% variable.names){
     variable.names = variable.names
   } else{
@@ -64,12 +135,13 @@ mvmr_horse = function(D, no_ini = 3, variable.names = "theta", n.iter = 10000, n
   for (j in 1:p){
     Tx[, ((j-1)*K+1):(j*K)] = diag(1 / Sx[j, ]^2)
   }
-  jags_fit = R2jags::jags(data = list(by = D$betaY, bx = Bx, sy = D$betaYse, Tx = Tx, N = p, K = K, R = diag(K)),
+  jags_fit = R2jags::jags.parallel(data = list(by = D$betaY, bx = Bx, sy = D$betaYse, Tx = Tx, N = p, K = K, R = diag(K)),
                           parameters.to.save = variable.names,
                           n.chains = no_ini,
                           n.iter = n.burnin + n.iter,
                           n.burnin = n.burnin,
-                          model.file = mvmr_horse_model)
+                          model.file = mvmr_horse_model_jags,
+                          n.cluster = n.chains)
   mr.coda = coda::as.mcmc(jags_fit)
   mr_estimate = data.frame("Parameter" = sprintf("theta[%i]", 1:K),
                            "Estimate" = round(unname(summary(mr.coda)$statistics[sprintf("theta[%i]", 1:K), 1]), 3),
